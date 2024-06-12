3aaf7133ff6c82d91120e50806041ac5c5ce368c
Catrina_Evilsizer <tranqcalypso@gmail.com>
2024-06-12 15:42:57 -0400

    added to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-var shapes = ["triangle", "square", "pentagon", "circle"];
 
-for (var x = 0; x < shapes.length; x++){
-    console.log(shapes[x]); 
-}/*
+for (let x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+/*
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {

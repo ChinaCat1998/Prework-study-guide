524bc87514b7f4f6e3da19fdeb5e77a48fd4ad21
Catrina_Evilsizer <tranqcalypso@gmail.com>
2024-06-12 15:36:43 -0400

    added commented out code

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,18 @@
-const topic = "HTML";
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var shapes = ["triangle", "square", "pentagon", "circle"];
 
-    const shapes = ["triangle", "square", "pentagon", "circle"];
-    for(let x = 0; x < shapes.length; x++) {
-     console.log(shapes[x]);
-    }
-const shapes = ["triangle", "square", "pentagon", "circle"];
-console.log(shapes[0]);
+for (var x = 0; x < shapes.length; x++){
+    console.log(shapes[x]); 
+}/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
